950c3ec523a6c3cc424a67e0bcea69a9b5c24077
star <star@cpe-172-119-50-55.socal.res.rr.com>
2018-07-09 15:13:41 +0800

    star 提交

---
+++ HelloWord.java
@@ -0,0 +1,14 @@
+package demon;
+
+public class HelloWord {
+	public static void main(String[] args) {
+//		for(int i=0;i<9;i++){
+//			System.out.println("Hello Word"+i);
+//		}
+		Schllo sch=new Schllo();
+		System.out.println("============="+sch.getAge());
+		System.out.println("============="+sch.getSg());
+	 }
+	
+
+}
