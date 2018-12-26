8cc82b87377c22137369b4247790e2c769bfbb31
star362 <star362@163.com>
2018-12-26 16:01:53 +0800

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -2,9 +2,9 @@ package demon;
 
 public class HelloWord {
 	public static void main(String[] args) {
-//		for(int i=0;i<9;i++){
-//			System.out.println("Hello Word"+i);
-//		}
+		for(int i=0;i<9;i++){
+			System.out.println("Hello Word"+i);
+		}
 		Schllo sch=new Schllo();
 		System.out.println("============="+sch.getAge());
 		System.out.println("============="+sch.getSg());
